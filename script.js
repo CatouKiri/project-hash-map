1a36ad51169d60c632e13cdada63cc4abeef1f83
Ed Benedict Quia <edbenedictquia@gmail.com>
2024-10-28 22:10:37 +0800

    add capacity and loadfactor

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-class HashMap {
+const capacity = 16;
+const loadFactor = 0.75;
 
-  }
+class HashMap {
+    constructor() {
+        this.buckets = new Array(capacity);
+        for (let i = 0; i < capacity; i++) {
+            this.buckets[i] = [];
+        }
+    }
+
+    hash(key, capacity) {
+        let hashCode = 0;
+        const primeNumber = 31;
+
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capacity;
+        }
+
+        return hashCode;
+    }
+
+    // set(key, value) {
+    // }
+}
+
+// function
